eb92e38f5645a4e449a5cfb333fd28c60050aa64
Paresh Maheshwari <Paresh-Maheshwari@users.noreply.github.com>
2026-05-03 17:11:42 +0530

    chore: update Gradle to 9.3.1, add androidx.annotation, switch to context-parameters [skip ci]

--- extensions/extension/build.gradle.kts
+++ extensions/extension/build.gradle.kts
@@ -4,4 +4,18 @@ extension {
 
 android {
     namespace = "app.paresh.extension"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.annotation)
 }
